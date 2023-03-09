ef897b28185d1f50bc47bf05a4486942f1fc6c9c
Manikanta Machapalli <Manikanta Machapalli>
2023-03-09 13:16:02 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def Jrntr(request):
+    return HttpResponse('Jrntr is one and only good alrounder in the tollywood')
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def harikrishna(request):
+    return HttpResponse("harikrishna gaaru is the father of jrntr")
